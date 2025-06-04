b506c32fe6c298e8bc0a35b07d51a75e57670bef
Satoshi <satoshighiba890@gmail.com>
2025-06-04 22:23:08 +0100

    feat: add basic expense entry UI
    add button, entry, and text for a basic expense entry UI

--- app/src/main/java/com/example/financial_broccoli/MainActivity.kt
+++ app/src/main/java/com/example/financial_broccoli/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.financial_broccoli.ui.theme.FinancialBroccoliTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.ui.text.style.TextAlign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +35,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FinancialBroccoliTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "World",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AddExpenseScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AddExpenseScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        var filledText by remember {mutableStateOf(value = "")}
+        val currencySymbol by remember { mutableStateOf("€")}
+        val errorText by remember { mutableStateOf("error") }
+
+        Text("Total: 0.0 $currencySymbol")
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row {
+            CurrencyInputField(
+                value = filledText,
+                onValueChange = {filledText = it},
+                label = "Enter your expense",
+                currencySymbol = currencySymbol,
+                isError = false,
+                errorText = errorText,
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            SumButton(
+                onClick = {/*TODO*/}
+            )
+
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FinancialBroccoliTheme {
-        Greeting("World")
+fun SumButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Text("Add")
     }
+}
+
+@Composable
+fun CurrencyInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    currencySymbol: String,
+    isError: Boolean,
+    errorText: String,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(label) },
+        suffix = { Text(currencySymbol) },
+        isError = isError,
+        supportingText = { if (isError) Text(errorText) },
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Decimal
+        ),
+        textStyle = LocalTextStyle.current.copy(
+            textAlign = TextAlign.Right
+        ),
+        modifier = modifier
+    )
 }
